55cd6ca64ee4649c8ee36f24a5c40687ec4b3c05
Jiang Sheng <jiangshengdev@outlook.com>
2025-02-15 13:09:19 +0800

    easy 10

--- exercises/easy/algorithm10.rs
+++ exercises/easy/algorithm10.rs
@@ -17,18 +17,39 @@ pub struct UndirectedGraph {
 }
 impl Graph for UndirectedGraph {
     fn new() -> UndirectedGraph {
+        // 创建无向图
         UndirectedGraph {
             adjacency_table: HashMap::new(),
         }
     }
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
+        // 返回可变引用
         &mut self.adjacency_table
     }
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
+        // 返回不可变引用
         &self.adjacency_table
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        // 同步实现无向边
+        if !self.contains(edge.0) {
+            // 如果节点 a 不存在则创建
+            self.add_node(edge.0);
+        }
+        if !self.contains(edge.1) {
+            // 如果节点 b 不存在则创建
+            self.add_node(edge.1);
+        }
+        // 获取可变邻接表
+        let map = self.adjacency_table_mutable();
+        // 在 a 的邻接向量新增 (b, 权重)
+        map.get_mut(edge.0)
+            .unwrap()
+            .push((edge.1.to_string(), edge.2));
+        // 在 b 的邻接向量新增 (a, 权重)
+        map.get_mut(edge.1)
+            .unwrap()
+            .push((edge.0.to_string(), edge.2));
     }
 }
 pub trait Graph {
@@ -36,11 +57,34 @@ pub trait Graph {
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
-        //TODO
+        // 检查是否已有此节点
+        if self.contains(node) {
+            return false;
+        }
+        // 新增空邻接向量
+        self.adjacency_table_mutable()
+            .insert(node.to_string(), Vec::new());
         true
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        // 确保节点 a 存在
+        if !self.contains(edge.0) {
+            self.add_node(edge.0);
+        }
+        // 确保节点 b 存在
+        if !self.contains(edge.1) {
+            self.add_node(edge.1);
+        }
+        // 获取可变邻接表
+        let map = self.adjacency_table_mutable();
+        // 在 a 中添加 (b, 权重)
+        map.get_mut(edge.0)
+            .unwrap()
+            .push((edge.1.to_string(), edge.2));
+        // 在 b 中添加 (a, 权重)
+        map.get_mut(edge.1)
+            .unwrap()
+            .push((edge.0.to_string(), edge.2));
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
