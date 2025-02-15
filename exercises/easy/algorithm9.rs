fa845c8adea089cdd38bad71908698365c9c89cf
Jiang Sheng <jiangshengdev@outlook.com>
2025-02-15 13:01:55 +0800

    easy 9

--- exercises/easy/algorithm9.rs
+++ exercises/easy/algorithm9.rs
@@ -36,7 +36,28 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        // 插入新值到末尾
+        self.items.push(value);
+        // 更新堆中元素个数
+        self.count += 1;
+        // 从新加入的节点开始向上调整
+        let mut idx = self.count;
+        // 开始上浮过程
+        while idx > 1 {
+            // 先计算父节点索引
+            let parent_idx = self.parent_idx(idx);
+            // 决定是否需要交换
+            let should_swap = (self.comparator)(&self.items[idx], &self.items[parent_idx]);
+            if should_swap {
+                // 执行交换操作
+                self.items.swap(idx, parent_idx);
+                // 更新当前节点索引
+                idx = parent_idx;
+            } else {
+                // 完成上浮
+                break;
+            }
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -56,8 +77,23 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-        0
+        // 获取左孩子索引
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+        // 如果右孩子存在，则两者之间选择比较优先的一个
+        if right <= self.count {
+            // 比较左右孩子，选择符合比较器规则的孩子
+            if (self.comparator)(&self.items[left], &self.items[right]) {
+                // 左孩子较优
+                left
+            } else {
+                // 右孩子较优
+                right
+            }
+        } else {
+            // 只有左孩子存在，直接返回左孩子索引
+            left
+        }
     }
 }
 
@@ -83,8 +119,35 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-        None
+        // 堆为空时返回 None
+        if self.count == 0 {
+            return None;
+        }
+        // 交换堆顶和末尾元素
+        self.items.swap(1, self.count);
+        // 弹出堆顶元素
+        let res = self.items.pop();
+        // 更新堆中元素个数
+        self.count -= 1;
+        // 从堆顶开始向下调整
+        let mut idx = 1;
+        // 当左孩子存在时进行下沉过程
+        while self.left_child_idx(idx) <= self.count {
+            // 选择左右孩子中比较优先的一个
+            let child_idx = self.smallest_child_idx(idx);
+            // 如果当前节点和孩子节点交换后能满足比较器规则，则交换
+            if (self.comparator)(&self.items[child_idx], &self.items[idx]) {
+                // 交换当前节点与选中孩子
+                self.items.swap(idx, child_idx);
+                // 更新当前节点索引为孩子索引
+                idx = child_idx;
+            } else {
+                // 调整完成，退出循环
+                break;
+            }
+        }
+        // 返回原堆顶元素
+        res
     }
 }
 
