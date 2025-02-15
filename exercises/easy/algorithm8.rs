097561716a3e145fbc7ca328ccb0df475da7e02c
Jiang Sheng <jiangshengdev@outlook.com>
2025-02-15 12:50:55 +0800

    easy 8

--- exercises/easy/algorithm8.rs
+++ exercises/easy/algorithm8.rs
@@ -51,29 +51,54 @@ impl<T> Default for Queue<T> {
     }
 }
 
-pub struct myStack<T> {
-    //TODO
+pub struct MyStack<T> {
     q1: Queue<T>,
     q2: Queue<T>,
 }
-impl<T> myStack<T> {
+
+impl<T> MyStack<T> {
     pub fn new() -> Self {
         Self {
-            //TODO
             q1: Queue::<T>::new(),
             q2: Queue::<T>::new(),
         }
     }
+
     pub fn push(&mut self, elem: T) {
-        //TODO
+        // 插入新元素到辅助队列 q2 中
+        self.q2.enqueue(elem);
+        // 将主队列 q1 中所有元素依次转移到 q2 中
+        while !self.q1.is_empty() {
+            // 从 q1 中移除第一个元素
+            match self.q1.dequeue() {
+                Ok(val) => {
+                    // 将移除的元素添加到 q2 中
+                    self.q2.enqueue(val);
+                }
+                Err(_) => {
+                    // 队列非空时不应失败，若出现异常直接退出循环
+                    break;
+                }
+            }
+        }
+        // 交换 q1 与 q2，确保栈顶元素始终在 q1 的头部
+        std::mem::swap(&mut self.q1, &mut self.q2);
     }
+
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-        Err("Stack is empty")
+        // 判断栈是否为空
+        if self.q1.is_empty() {
+            // 栈空时返回错误
+            Err("Stack is empty")
+        } else {
+            // 栈非空时，从 q1 中移除并返回元素
+            self.q1.dequeue()
+        }
     }
+
     pub fn is_empty(&self) -> bool {
-        //TODO
-        true
+        // 返回 q1 是否为空，空表示栈为空
+        self.q1.is_empty()
     }
 }
 
@@ -83,7 +108,7 @@ mod tests {
 
     #[test]
     fn test_queue() {
-        let mut s = myStack::<i32>::new();
+        let mut s = MyStack::<i32>::new();
         assert_eq!(s.pop(), Err("Stack is empty"));
         s.push(1);
         s.push(2);
